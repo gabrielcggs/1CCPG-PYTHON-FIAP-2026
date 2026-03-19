ef3b08ecd09413b897b3f76982569e124da3954a
gabrielcggs <gabrielcggsilva@gmail.com>
2026-03-19 15:31:58 -0300

    atividade aula02

---
+++ aula02-variáveis-operadores/aula02-atividade.py
@@ -0,0 +1,20 @@
+# ATIVIDADE 1
+
+nome = input("Digite o seu nome:")
+print(f"Seja bem vindo(a) {nome}")
+
+
+# ATIVIDADE 2
+
+nascimento = input("Digite a sua data de nascimento (dd/mm/aaaa):")
+print(f"Nascimento: {nascimento}")
+
+print(nome, nascimento)
+
+
+# ATIVIDADE 3
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+soma = num1 + num2
+print(f"A soma é: {soma}")
